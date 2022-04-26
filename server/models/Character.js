abb165f761dd54184d31767b7e591596db9eaf53
cody-junier <codyjunier@gmail.com>
2022-04-26 13:14:18 -0700

    User + character datatypes

--- server/models/Character.js
+++ server/models/Character.js
@@ -4,7 +4,58 @@ class Character extends Model {}
 
 Character.init(
     {
-        id:{},
+        id:{
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        characterName:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        characterAge:{
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        },
+        characterClass:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        characterRace:{
+            type: DataTypes.STRING,
+            allowNull: true
+
+        },
+        franchise:{
+            type: DataTypes.STRING,
+            allowNull: true
+
+        },
+        era:{
+            type: DataTypes.STRING,
+            allowNull: true
+
+        },
+        img_url:{
+            type: DataTypes.STRING,
+            allowNull: true
+
+        },
+        description:{
+            type: DataTypes.TEXT,
+            allowNull: true
+
+        },
+        // adult?
+        over18:{
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    },
+    {
+        // hooks
+        sequelize,
     }
 )
 
--- server/models/User.js
+++ server/models/User.js
@@ -4,11 +4,42 @@ class User extends Model {}
 
 User.init(
     {
-        id:{},
-        email:{},
-        password:{},
-        firstName:{},
-        lastName:{},
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email:{
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            // email validation
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            // password length? validataion
+        },
+        firstName:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        lastName:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+          },
+    },
+    {
+        // hooks:{}
+        sequelize,
+        freezeTableName: true,
+        modelName: 'user'
     }
 )
 
