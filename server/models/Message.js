3af1643389495802ea82e9dbad4d2751ccab9e4e
cody-junier <codyjunier@gmail.com>
2022-04-26 13:21:37 -0700

    leg work for message data types + tables

--- server/models/Message.js
+++ server/models/Message.js
@@ -4,7 +4,29 @@ class Message extends Model {}
 
 Message.init(
     {
-        id:{},
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1],
+            },
+          },
+        //   sender
+        // recipient
+        // has read?
+        
+    },
+    {
+        // hooks
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelName: 'message'
     }
 )
 
