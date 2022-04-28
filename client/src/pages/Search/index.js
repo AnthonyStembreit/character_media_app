3edfa4d210281eec0ffb4d4aaa79f2bac36e50e3
AnthonyStembreit <stembreit@gmail.com>
2022-04-27 19:02:13 -0500

    created basic jsx for search page, added styles for upon landing as well as a back ground image

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
-
+import { useSelector } from 'react-redux';
+import "./search.css";
 export default function Search() {
     const state = useSelector(state => state);
     console.log(state.auth)
     return (
-        <div>
-            Search
-        </div>
+        <section id="search">
+            <div id="background-img">
+                <h1>Search</h1>
+                <form id="searchForm">
+                    <input type="text" id="search-value" />
+                    <select id="search-type">
+                        <option>Choose One</option>
+                        <option>Character Name</option>
+                        <option>Franchize</option>
+                        <option>Username</option>
+                    </select>
+                </form>
+                <p>Connect with others! -Search by character name, franchize, or username!</p>
+            </div>
+            <div id="results">
+
+            </div>
+        </section>
     )
 }
