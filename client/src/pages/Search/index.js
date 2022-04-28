d0887a4ee6dc6acbd347c5be77568d08ece08532
AnthonyStembreit <stembreit@gmail.com>
2022-04-27 19:09:54 -0500

    now console logging user input from search form

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -4,11 +4,21 @@ import "./search.css";
 export default function Search() {
     const state = useSelector(state => state);
     console.log(state.auth)
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        let searchInput = document.getElementById("search-value").value 
+        let searchType = document.getElementById("search-type").value
+        console.log(searchInput, searchType)
+        //TODO call axios here to get results from db 
+        //and then present them on the page
+        document.getElementById("search-value").value = ""
+        document.getElementById("search-type").value = "Choose One"
+    }
     return (
         <section id="search">
             <div id="background-img">
                 <h1>Search</h1>
-                <form id="searchForm">
+                <form onSubmit={(e) => handleSearch(e)} id="searchForm">
                     <input type="text" id="search-value" />
                     <select id="search-type">
                         <option>Choose One</option>
