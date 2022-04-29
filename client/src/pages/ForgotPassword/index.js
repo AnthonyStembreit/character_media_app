fce22cb13eb8f3613a890eafbe5d193967b2cd1d
AnthonyStembreit <stembreit@gmail.com>
2022-04-29 13:52:53 -0500

    now console loggin values from forms on sending reset email and password reset

--- client/src/pages/ForgotPassword/index.js
+++ client/src/pages/ForgotPassword/index.js
@@ -4,16 +4,32 @@ import { useSelector } from 'react-redux'
 export default function ForgotPassword() {
     const state = useSelector(state => state);
     console.log(state.auth)
+    const handleResetEmail = (e) => {
+        e.preventDefault();
+        const email = document.getElementById("resetEmail").value.trim()
+        const confirmEmail = document.getElementById("confirmEmail").value.trim()
+        if (email && confirmEmail) {
+            if(email === confirmEmail){
+                console.log(email, confirmEmail)
+                //axios call here
+            }
+            else{
+                alert("Emails must match!")
+            }
+        } else {
+            alert("Must enter email in both fields!")
+        }
+    }
     return (
         <div>
-           <h2>Forgot Password??</h2>
-           <form>
-               <label htmlFor="resetEmail">Email</label>
-               <input id="resetEmail" />
-               <label htmlFor="confirmEmail">Confirm Email</label>
-               <input id="confirmEmail"/>
-               <button>Send Reset Email</button>
-           </form>
+            <h2>Forgot Password??</h2>
+            <form onSubmit={(e) => { handleResetEmail(e) }}>
+                <label htmlFor="resetEmail">Email</label>
+                <input id="resetEmail" />
+                <label htmlFor="confirmEmail">Confirm Email</label>
+                <input id="confirmEmail" />
+                <button>Send Reset Email</button>
+            </form>
         </div>
     )
 }
