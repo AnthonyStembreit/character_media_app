2976f89feb5871d929140259e5762f913de82638
AnthonyStembreit <stembreit@gmail.com>
2022-05-25 23:28:37 -0500

    file path correction

--- server/server.js
+++ server/server.js
@@ -14,10 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 // We need to use sessions to keep track of our user's login status
